1a06c539023bff055d170ab1e21767d8a0c7be5d
OsorioJc-696 <e_2019200661k@uncp.edu.pe>
2023-10-30 08:39:58 -0500

    new

--- script.js
+++ script.js
@@ -12,6 +12,121 @@ function mostrarOcultarMenu(){
     }
 }
 
+// CHAT BOT
+document.addEventListener("DOMContentLoaded", function () {
+  const chatbotIcon = document.getElementById("chatbot-icon");
+  const chatbot = document.getElementById("chatbot");
+  const closeChatbot = document.getElementById("close-chatbot");
+  const chatMessages = document.getElementById("chat-messages");
+  const userInput = document.getElementById("user-input");
+  const sendButton = document.getElementById("send-button");
+
+  chatbotIcon.addEventListener("click", function () {
+      chatbot.style.display = "block";
+      chatbotIcon.style.display = "none";
+      // Muestra un mensaje de bienvenida
+      displayBotResponse("¡Hola! Soy el chatbot de la inmobiliaria. ¿En qué puedo ayudarte?");
+      // Muestra las opciones para realizar consultas
+      displayBotResponse("Elige una opción:");
+      displayBotResponse("1. ¿Cuál es el horario de oficina");
+      displayBotResponse("2. Quiero contactar con un Agente Adeministrativo");
+      displayBotResponse("3. Quiero comprar una propiedad");
+  });
+
+  closeChatbot.addEventListener("click", function () {
+      chatbot.style.display = "none";
+      chatbotIcon.style.display = "block";
+  });
+
+
+  function sendMessage(message) {
+      const userMessage = document.createElement("div");
+      userMessage.className = "user-message";
+
+  // Icono del usuario
+  const userIcon = document.createElement("div");
+  userIcon.className = "user-icon";
+  userIcon.innerHTML = '<img src="user-icon.png" alt="User Icon">';
+  
+
+  // Contenido del mensaje
+  const messageContent = document.createElement("div");
+  messageContent.textContent = message;
+
+
+  userMessage.appendChild(userIcon);
+  userMessage.appendChild(messageContent);
+
+  chatMessages.appendChild(userMessage);
+
+      const botResponse = getBotResponse(message);
+      displayBotResponse(botResponse);
+  }
+
+  function getBotResponse(userMessage) {
+    if (userMessage.includes("1")) {
+      return "Nuestro horario es: "
+      +"\nlun:	Abierto las 24 horas"
+      +"\nmar:	9:00 a. m. – 7:00 p. m."
+      +"\nmié:	9:00 a. m. – 7:00 p. m."
+      +"\njue:	9:00 a. m. – 7:00 p. m."
+      +"\nvie:	9:00 a. m. – 7:00 p. m."
+      +"\nsáb:	9:00 a. m. – 1:00 p. m."
+      +"\ndom:	Cerrado";
+  } else if (userMessage.includes("2")) {
+      return "Puedes contactarnos por:"
+      +"\n Teléfono: 947 278 720"
+      +"\n Correo electrónico: Ventas@inversionesurcons.com ";
+  } else if (userMessage.includes("3")) {
+      return "Aquí tienes algunas preguntas frecuentes:\n1. ¿Cuáles son los requisitos para alquilar una propiedad?\n2. ¿Cuál es el proceso de compra de una propiedad?\n3. ¿Cuáles son las zonas disponibles?";
+  } else {
+      return "Por favor, selecciona una de las opciones (1, 2 o 3) para realizar una consulta.";
+  }
+}
+
+function displayBotResponse(response) {
+  const botMessage = document.createElement("div");
+  botMessage.className = "chatBot-message";
+  
+  // Icono del chatbot
+  const chatbotIcon = document.createElement("div");
+  chatbotIcon.className = "chatBot-icon";
+  chatbotIcon.innerHTML = '<img src="chatbot-icon.png" alt="Chatbot Icon">';
+  
+  // Contenido del mensaje
+  const messageContent = document.createElement("div");
+  messageContent.textContent = response;
+
+  botMessage.appendChild(chatbotIcon);
+  botMessage.appendChild(messageContent);
+
+  chatMessages.appendChild(botMessage);
+}
+
+sendButton.addEventListener("click", function () {
+    const message = userInput.value;
+    if (message.trim() !== "") {
+        sendMessage(message);
+        userInput.value = "";
+    }
+  });
+ 
+
+  userInput.addEventListener("keyup", function (event) {
+      if (event.key === "Enter") {
+          sendButton.click();
+      }
+  });
+
+  function scrollToBottom() {
+      chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+});
+
+
+
+
+
 
 
 
@@ -42,6 +157,9 @@ function ocultarTodasSubpestañas() {
       subpestaña.style.display = "none";
   }
 }
+
+
+
 
 // Llama a la función para ocultar todas las subpestañas al cargar la página
 ocultarTodasSubpestañas();
@@ -170,58 +288,58 @@ particlesJS('particles-js', {
   });
 
 //MOSTRAR SECCIONES
-document.addEventListener("DOMContentLoaded", function () {
-  const sections = document.querySelectorAll("section");
-  let autoScroll = true;
-  let scrollDelay = 5000; // 4 segundos
-  let stopScrollTimeout = null;
+// document.addEventListener("DOMContentLoaded", function () {
+//   const sections = document.querySelectorAll("section");
+//   let autoScroll = true;
+//   let scrollDelay = 10000; // 4 segundos
+//   let stopScrollTimeout = null;
 
-  function scrollToNextSection() {
-      if (autoScroll) {
-          let currentSection = document.querySelector(".active");
-          let nextSection = currentSection.nextElementSibling;
+//   function scrollToNextSection() {
+//       if (autoScroll) {
+//           let currentSection = document.querySelector(".active");
+//           let nextSection = currentSection.nextElementSibling;
 
-          if (!nextSection) {
-              nextSection = sections[0];
-          }
+//           if (!nextSection) {
+//               nextSection = sections[0];
+//           }
 
-          currentSection.classList.remove("active");
-          nextSection.classList.add("active");
+//           currentSection.classList.remove("active");
+//           nextSection.classList.add("active");
 
-          let scrollTo = nextSection.offsetTop - 80; // 80px de compensación hacia abajo
-          window.scrollTo({
-              top: scrollTo,
-              behavior: "smooth"
-          });
+//           let scrollTo = nextSection.offsetTop - 80; // 80px de compensación hacia abajo
+//           window.scrollTo({
+//               top: scrollTo,
+//               behavior: "smooth"
+//           });
 
-          stopScrollTimeout = setTimeout(function () {
-              scrollToNextSection();
-          }, scrollDelay);
-      }
-  }
+//           stopScrollTimeout = setTimeout(function () {
+//               scrollToNextSection();
+//           }, scrollDelay);
+//       }
+//   }
 
-  function pauseScrollOnMouseOver() {
-      autoScroll = false;
-      if (stopScrollTimeout) {
-          clearTimeout(stopScrollTimeout);
-      }
-  }
+//   function pauseScrollOnMouseOver() {
+//       autoScroll = false;
+//       if (stopScrollTimeout) {
+//           clearTimeout(stopScrollTimeout);
+//       }
+//   }
 
-  function resumeScrollOnMouseOut() {
-      autoScroll = true;
-      stopScrollTimeout = setTimeout(function () {
-          scrollToNextSection();
-      }, 8000); // 6 segundos
-  }
+//   function resumeScrollOnMouseOut() {
+//       autoScroll = true;
+//       stopScrollTimeout = setTimeout(function () {
+//           scrollToNextSection();
+//       }, 12000); // 6 segundos
+//   }
 
-  sections[0].classList.add("active");
-  scrollToNextSection();
+//   sections[0].classList.add("active");
+//   scrollToNextSection();
 
-  sections.forEach((section) => {
-      section.addEventListener("mouseover", pauseScrollOnMouseOver);
-      section.addEventListener("mouseout", resumeScrollOnMouseOut);
-  });
-});
+//   sections.forEach((section) => {
+//       section.addEventListener("mouseover", pauseScrollOnMouseOver);
+//       section.addEventListener("mouseout", resumeScrollOnMouseOut);
+//   });
+// });
 
 
 
